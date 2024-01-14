c831c499c993b01d166fbab3bd589bd797eca219
Javier Salazar Cavazos <javiersc@umich.edu>
2024-01-14 17:50:49 -0500

    Update fedora-hyprland.sh

--- fedora-hyprland.sh
+++ fedora-hyprland.sh
@@ -26,31 +26,48 @@ sudo dnf install -y polkit-gnome
 # screenshot environment variable HYPRSHOT_DIR set to ~/Pictures/Screenshots in hypr.conf
 
 # hyprpaper config (wallpaper)
-cp ~/linux/configs/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+mkdir ~/.config/hypr
+cp ~/linux/configs/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 cp ~/linux/files/astro.png ~/Pictures/wallpapers/astro.png
 
 # pywal get color schemes
 # wal -i "~/Pictures/wallpapers/astro.png"
 
 # hyprland config
-cp ~/linux/configs/hypr/hyprland.conf ~/.configs/hypr/hyprland.conf
+cp ~/linux/configs/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
 
 # waybar config
+mkdir ~/.config/waybar
 cp ~/linux/configs/waybar/config.jsonc ~/.config/waybar/config.jsonc
 cp ~/linux/configs/waybar/style.css ~/.config/waybar/style.css
 
 # rofi config
+mkdir ~/.config/rofi
+mkdir ~/.local/share/rofi
+mkdir ~/.local/share/rofi/themes
 cp ~/linux/configs/rofi/config.rasi ~/.config/rofi/config.rasi
 cp ~/linux/configs/rofi/nord.rasi ~/.local/share/rofi/themes/nord.rasi
 
 # wlogout theme
-cp ~/linux/configs/wlogout ~/.config/wlogout
+mkdir ~/.config/wlogout
+cp -R ~/linux/configs/wlogout ~/.config/
 
 # swaylock config
+mkdir ~/.swaylock
 cp ~/linux/configs/swaylock/config ~/.swaylock/config
 
 # polkit script
+mkdir ~/.config/polkit
 cp ~/linux/configs/polkit/script.sh ~/.config/polkit/script.sh
+
+# nerd font setup
+sudo dnf install -y fira-code-fonts
+mkdir -p $HOME/Downloads/nerdfonts/
+cd $HOME/Downloads/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/CascadiaCode.zip
+unzip '*.zip' -d $HOME/Downloads/nerdfonts/
+rm -rf *.zip
+sudo cp -R $HOME/Downloads/nerdfonts/ /usr/share/fonts/
 
 
 
